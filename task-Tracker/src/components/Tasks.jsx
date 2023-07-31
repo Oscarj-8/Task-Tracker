1e7e081ca63f5ac663f6d229cc55c5acedc17e93
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-07-31 17:16:54 +0300

    Resolve dep issues

--- task-Tracker/src/components/Tasks.jsx
+++ task-Tracker/src/components/Tasks.jsx
@@ -2,11 +2,11 @@ import Task from "./Task";
 
 const Tasks = ({ tasks }) => {
   return (
-    <>
+    <div className="tasks-order">
       {tasks.map((task) => (
         <Task key={task.id} task={task} />
       ))}
-    </>
+    </div>
   );
 };
 
