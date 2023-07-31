7d01a283b05f17d236b2646e65b1d5f166c8c923
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-08-01 00:22:21 +0300

    Add reminder

--- task-Tracker/src/App.jsx
+++ task-Tracker/src/App.jsx
@@ -9,25 +9,25 @@ function App() {
       id: 1,
       text: "Doctor's Appointment",
       day: "Feb 5th at 2:30pm",
-      remider: true,
+      reminder: true,
     },
     {
       id: 2,
       text: "Meeting at school",
       day: "Aug 5th at 8:30am",
-      remider: true,
+      reminder: true,
     },
     {
       id: 3,
       text: "Food Shopping",
       day: "Feb 5th at 9:30pm",
-      remider: true,
+      reminder: true,
     },
     {
       id: 4,
       text: "Football Match",
       day: "Feb 5th at 10:30pm",
-      remider: false,
+      reminder: false,
     },
   ]);
 
@@ -35,12 +35,24 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <div className="tracker-box">
         <Header title="Task Tracker" />
         {tasks.length > 0 ? (
-          <Tasks tasks={tasks} onDelete={deleteTask} />
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />
         ) : (
           "No tasks to show"
         )}
